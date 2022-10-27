1e4d20d5a57b5b9b5d6c3475dcd1b2caa5ba0a1f
unknown <kevin@kchn.com.au>
2022-10-27 16:01:53 +1100

    Contacts list rendered with sorted groups of letters

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,125 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { onMounted, ref } from "vue";
+import axios from "axios";
+import type { Contact } from "./models/api";
+import ContactCard from "./components/ContactCard.vue";
+
+/*=============================================
+=            Contacts list fetch and sort            =
+=============================================*/
+
+// define a FilteredContactsArray that stores the character it is filtering by and the array of results
+
+type FilteredContactsArray = {
+    filteredCharacter: string;
+    contacts: Array<Contact>;
+};
+
+let filteredContactsArrays = ref<Array<FilteredContactsArray>>([]);
+
+/**
+ * Generate a list of FileredContactArray entities from an unfiltered list.
+ * @param contacts master array of unfiltered contacts
+ */
+const generateFilteredContactsArraysFromMasterList = (
+    contacts: Array<Contact>
+): Array<FilteredContactsArray> => {
+    //
+    // generate a list of first letters in contact.name
+    const lettersSet = new Set(contacts.map((contact) => contact.name[0]));
+
+    // init array of FilteredContactsArray
+    const resultingArray: Array<FilteredContactsArray> = [];
+
+    // function to sort by alphabet
+    const compareStrings = (a: string, b: string): number => {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+    };
+
+    // for each value in the set, make a new FilteredContactsArray
+    lettersSet.forEach((letter) => {
+        const filteredContactsArray: FilteredContactsArray = {
+            filteredCharacter: letter,
+            contacts: contacts
+                .filter((contact) => contact.name[0] == letter)
+                .sort((a, b) => compareStrings(a.name, b.name)),
+        };
+
+        resultingArray.push(filteredContactsArray);
+    });
+
+    // sort the resulting array by the filtered letter
+    resultingArray.sort((a, b) =>
+        compareStrings(a.filteredCharacter, b.filteredCharacter)
+    );
+
+    return resultingArray;
+};
+
+// init function
+
+const init = async () => {
+    const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+
+    filteredContactsArrays.value = generateFilteredContactsArraysFromMasterList(
+        response.data
+    );
+};
+
+// onmount hook
+onMounted(() => {
+    init();
+});
+
+/*=====  End of Contacts list fetch and sort  ======*/
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <main class="content-wrapper">
+        <h1 class="font-light">Contacts</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+        <div class="contacts-list">
+            <template
+                v-for="(filteredContactsArray, index) in filteredContactsArrays"
+                :key="index"
+            >
+                <h2>{{ filteredContactsArray.filteredCharacter }}</h2>
+                <ContactCard
+                    v-for="contact in filteredContactsArray.contacts"
+                    :contact="contact"
+                    :key="contact.id"
+                />
+            </template>
+        </div>
+    </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+/* Utility classes */
+
+.font-light {
+    font-weight: 300;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+/* Layout and content classes */
+.content-wrapper {
+    max-width: 57rem;
+    padding: 2rem;
+    margin-inline: auto;
+    min-height: 100vh;
+    background-color: #f7f7f8;
+    /* box-shadow: 5px 5px 15px #19191919; */
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.contacts-list {
+    margin-top: 2rem;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    flex-direction: column;
+    row-gap: 1rem;
 }
 </style>
---
+++ src/components/ContactCard.vue
@@ -0,0 +1,53 @@
+<!-- Display a contact as a card -->
+
+<script setup lang="ts">
+import type { Contact } from "@/models/api";
+import { defineProps } from "vue";
+
+// define props
+
+defineProps<{
+    contact: Contact;
+}>();
+
+//  define emits
+defineEmits(["click"]);
+</script>
+
+<template>
+    <div class="card" @click="$emit('click')">
+        <h3 class="font-semibold">{{ contact.name }}</h3>
+        <p class="desktop-only asdf">Phone: {{ contact.phone }}</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+/* Utility classes */
+
+.font-semibold {
+    font-weight: 600;
+}
+
+.desktop-only {
+    display: none;
+}
+
+@media screen and (min-width: 768px) {
+    .desktop-only {
+        display: block;
+    }
+}
+
+/* Styling */
+.card {
+    padding: 1rem;
+    background-color: white;
+
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.card:hover {
+    outline: 3px solid lightblue;
+}
+</style>
